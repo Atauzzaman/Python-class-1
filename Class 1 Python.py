bcd657b16f743def0821fe64b21f2794839115df
Atauzzaman <120796680+Atauzzaman@users.noreply.github.com>
2022-12-17 06:55:28 -0800

    Add files via upload

---
+++ Class 1 Python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import Image
+Image('gradiant_descent.jpg')
+
+
+# In[4]:
+
+
+import pandas as atauzzman
+df = atauzzman.read_excel('global_laptop_selling_data.xlsx')
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
